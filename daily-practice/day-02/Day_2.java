dceb08b247b183f8b78630e70ce99b9988f1a843
pavan-200623 <pavandhanawade922@gmail.com>
2026-09-16 09:09:58 +0530

    Day 02: prime number

---
+++ daily-practice/day-02/Day_2.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Day_2 {
+
+    public static void main(String[] args){
+        Scanner  s = new Scanner(System.in);
+        System.out.println("enter the nuber:");
+        int n=s.nextInt();
+    int count=0;
+    for(int i=1;i<=n;i++){
+        if(n%i==0){
+            count++;}
+    }
+    if (count==2){
+        System.out.println("given number is prime");
+    }
+    else{
+        System.out.println("given number is not prime");
+    }
+}
+    }
+    
+
